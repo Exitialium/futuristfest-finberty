a23423dfd08adec1ecc9929e9e22c6554a656e6c
Exitialium <henrywu0424@gmail.com>
2021-10-09 16:59:56 -0400

    Added dropdown for algortihms

--- app.py
+++ app.py
@@ -8,12 +8,26 @@ df = px.data.stocks()
 
 app = dash.Dash(__name__)
 
+algorithmDict = {
+  "Trend-following Strategies": "tfs",
+  "Arbitrage Opportunities": "ao",
+  "Index Fund Rebalancing": "ifr",
+  "Mathematical Model-based Strategies": "mms"
+}
+
 app.layout = html.Div([
     dcc.Dropdown(
         id="ticker",
         options=[{"label": x, "value": x} 
                  for x in df.columns[1:]],
         value=df.columns[1],
+        clearable=False,
+    ),
+    dcc.Dropdown(
+        id="Algorithm",
+        options=[{"label": x, "value": algorithmDict[x]} 
+                 for x in algorithmDict],
+        value="tfs",
         clearable=False,
     ),
     dcc.Graph(id="time-series-chart"),
